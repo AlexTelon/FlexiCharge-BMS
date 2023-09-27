f0018d311e980e307ecdc315e3ae37433d420f3b
ViktorM02 <viktormann8@gmail.com>
2023-09-27 18:45:16 +0200

    Adjust focus of function convert adc to volt

--- bms_software/Core/Src/bms_current_reader.c
+++ bms_software/Core/Src/bms_current_reader.c
@@ -5,8 +5,13 @@
 
 #include "bms_current_reader.h"
 
-float convert_adc_to_volt(uint16_t adc_value){
-	return (SYSTEM_VOLTAGE*adc_value)/ADC_RESOLUTION;
+/*
+ * Converts the ADC to m amp
+ */
+float convert_adc_to_mAmp(uint16_t adc_value){
+	float voltage = (SYSTEM_VOLTAGE*adc_value)/ADC_RESOLUTION;
+	float mAmp = (voltage * CHARGING_RESISTANCE) * MILLI;
+	return mAmp;
 }
 
 
