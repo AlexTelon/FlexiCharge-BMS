bc7076c13e55cc6369128129659b64793518f8f7
ViktorM02 <viktormann8@gmail.com>
2023-09-27 19:01:57 +0200

    rename input in convert_adc_to_amp

--- bms_software/Core/Inc/bms_current_reader.h
+++ bms_software/Core/Inc/bms_current_reader.h
@@ -12,6 +12,6 @@
 uint16_t adc_resistor_drop(uint16_t adc_pre, uint16_t adc_post);
 
 //Converts the ADC to m amp
-float convert_adc_to_volt(uint16_t adc_value);
+float convert_adc_to_volt(uint16_t adc_diff);
 
 #endif /* INC_BMS_CURRENT_READER_H_ */
