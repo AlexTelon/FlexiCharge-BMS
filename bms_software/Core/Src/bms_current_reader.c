8f411e7a077a261d9adaa7c4729da3ebbf216bef
ViktorM02 <viktormann8@gmail.com>
2023-09-27 19:00:06 +0200

    Add function adc drop

--- bms_software/Core/Src/bms_current_reader.c
+++ bms_software/Core/Src/bms_current_reader.c
@@ -5,9 +5,11 @@
 
 #include "bms_current_reader.h"
 
-/*
- * Converts the ADC to m amp
- */
+
+uint16_t adc_resistor_drop(uint16_t adc_pre, uint16_t adc_post){
+	return adc_pre - adc_post;
+}
+
 float convert_adc_to_mAmp(uint16_t adc_value){
 	float voltage = (SYSTEM_VOLTAGE*adc_value)/ADC_RESOLUTION;
 	float mAmp = (voltage * CHARGING_RESISTANCE) * MILLI;
