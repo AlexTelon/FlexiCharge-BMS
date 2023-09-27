a3432641c065e841d363570f63c5a3be67815789
ViktorM02 <viktormann8@gmail.com>
2023-09-27 18:06:12 +0200

    Implement adc to voltage converter
    Make file for storing common constants

--- bms_software/Core/Inc/bms_cell_voltage_reader.h
+++ bms_software/Core/Inc/bms_cell_voltage_reader.h
@@ -4,12 +4,7 @@
 #define INC_BMS_CELL_VOLTAGE_READER_H_
 
 #include "stm32f4xx_hal.h"
-
-#define ADC_RESOLUTION 4095 // The MCU has a 12 bit adc.
-#define SYSTEM_VOLTAGE 3.3 // The MCU runs on 3.3 volt.
-#define VOLTAGE_SCALAR 1272 // Since the voltage will be represented as a 16 bit unsigned integer, we need to scale it up to get more accuracy. This scalar also up-scales the voltage read. With this value, the voltage output will represent the voltage at the battery.
-#define VOLTAGE_READING_UPDATE_DELAY 300 // A delay, in miliseconds, between voltage readings.
-
+#include "bms_constats.h"
 
 enum battery_pack // An enum to allow users to specify which cell to be read.
 {
--- bms_software/Core/Inc/bms_current_reader.h
+++ bms_software/Core/Inc/bms_current_reader.h
@@ -6,6 +6,9 @@
 #ifndef INC_BMS_CURRENT_READER_H_
 #define INC_BMS_CURRENT_READER_H_
 
+#include "stm32f4xx_hal.h"
+#include "bms_constats.h"
 
+float convert_adc_to_volt(uint16_t adc_value);
 
 #endif /* INC_BMS_CURRENT_READER_H_ */
