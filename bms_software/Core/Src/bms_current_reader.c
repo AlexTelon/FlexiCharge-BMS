fd5c838edf6dabbf1fad94a4c0dfbee874d6dcf3
ViktorM02 <viktormann8@gmail.com>
2023-09-27 19:01:57 +0200

    rename input in convert_adc_to_amp

--- bms_software/Core/Src/bms_current_reader.c
+++ bms_software/Core/Src/bms_current_reader.c
@@ -10,8 +10,8 @@ uint16_t adc_resistor_drop(uint16_t adc_pre, uint16_t adc_post){
 	return adc_pre - adc_post;
 }
 
-float convert_adc_to_mAmp(uint16_t adc_value){
-	float voltage = (SYSTEM_VOLTAGE*adc_value)/ADC_RESOLUTION;
+float convert_adc_to_mAmp(uint16_t adc_diff){
+	float voltage = (SYSTEM_VOLTAGE*adc_diff)/ADC_RESOLUTION;
 	float mAmp = (voltage * CHARGING_RESISTANCE) * MILLI;
 	return mAmp;
 }
