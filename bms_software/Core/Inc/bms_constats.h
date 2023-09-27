2c1f2311dadd02b0f52d3e390c95cc2ed130ce5e
ViktorM02 <viktormann8@gmail.com>
2023-09-27 18:19:16 +0200

    Add constants

--- bms_software/Core/Inc/bms_constats.h
+++ bms_software/Core/Inc/bms_constats.h
@@ -10,12 +10,15 @@
 #define ADC_RESOLUTION 4095 // The MCU has a 12 bit adc.
 #define SYSTEM_VOLTAGE 3.3 // The MCU runs on 3.3 volt.
 
-
 /* Since the voltage will be represented as a 16 bit unsigned integer, we need
  * to scale it up to get more accuracy. This scalar also up-scales the voltage
  * read.With this value, the voltage output will represent the voltage at the
  * battery.
 */
 #define VOLTAGE_SCALAR 1272
+
+// The resistance in the charging circuit for reading the current to the batteries
+#define CHARGING_RESISTANCE 5
+#define MILLI 1000
 
 #endif /* INC_BMS_CONSTATS_H_ */
