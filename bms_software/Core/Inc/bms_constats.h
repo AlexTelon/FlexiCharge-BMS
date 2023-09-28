104e2180dd288a20b43c730f5ea7d74da6942054
ViktorM02 <viktormann8@gmail.com>
2023-09-28 13:27:53 +0200

    Change charging_resistance value

--- bms_software/Core/Inc/bms_constats.h
+++ bms_software/Core/Inc/bms_constats.h
@@ -18,7 +18,7 @@
 #define VOLTAGE_SCALAR 1272
 
 // The resistance in the charging circuit for reading the current to the batteries
-#define CHARGING_RESISTANCE 5
+#define CHARGING_RESISTANCE 5.4
 #define MILLI 1000
 
 #endif /* INC_BMS_CONSTATS_H_ */
