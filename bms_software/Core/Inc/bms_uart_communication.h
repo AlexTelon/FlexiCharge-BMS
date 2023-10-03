8b29826f6fee3b2c9e517fc691793ae75efe6669
KevFord <81682161+KevFord@users.noreply.github.com>
2023-10-03 17:16:00 +0200

    Add source and header file for new functionality.

---
+++ bms_software/Core/Inc/bms_uart_communication.h
@@ -0,0 +1,13 @@
+/*
+ * bms_uart_communication.h
+ *
+ *  Created on: Oct 3, 2023
+ *      Author: Kevin
+ */
+
+#ifndef INC_BMS_UART_COMMUNICATION_H_
+#define INC_BMS_UART_COMMUNICATION_H_
+
+
+
+#endif /* INC_BMS_UART_COMMUNICATION_H_ */
---
+++ bms_software/Core/Src/bms_uart_communication.c
@@ -0,0 +1,8 @@
+/*
+ * bms_uart_communication.c
+ *
+ *  Created on: Oct 3, 2023
+ *      Author: Kevin
+ */
+
+
