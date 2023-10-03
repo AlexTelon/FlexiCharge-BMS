105cbc1f7ee52068cb3995dd24045f16b9e91fa1
ViktorM02 <mavi21mw@student.ju.se>
2023-10-03 23:49:42 +0200

    add charging files

---
+++ bms_software/Core/Inc/bms_charging.h
@@ -0,0 +1,13 @@
+/*
+ * bms_charging.h
+ *
+ *  Created on: Oct 3, 2023
+ *      Author: viram
+ */
+
+#ifndef INC_BMS_CHARGING_H_
+#define INC_BMS_CHARGING_H_
+
+
+
+#endif /* INC_BMS_CHARGING_H_ */
---
+++ bms_software/Core/Src/bms_charging.c
@@ -0,0 +1,8 @@
+/*
+ * bms_charging.c
+ *
+ *  Created on: Oct 3, 2023
+ *      Author: viram
+ */
+
+
