1dab689675ce6b0881b0bbf238beaf56a008bfc4
KevFord <81682161+KevFord@users.noreply.github.com>
2023-10-04 07:15:54 +0200

    Add all commands to be sent over uart and write a function to transmit strings via uart.

--- bms_software/Core/Inc/bms_uart_communication.h
+++ bms_software/Core/Inc/bms_uart_communication.h
@@ -6,6 +6,20 @@
 #ifndef INC_BMS_UART_COMMUNICATION_H_
 #define INC_BMS_UART_COMMUNICATION_H_
 
+#include "main.h"
+
+// All the commands that will be sent to and from the BMS.
+const char* ok = "ok";
+const char* voltage = "voltage";
+const char* begin = "begin";
+const char* end = "end";
+const char* charge = "charge";
+const char* temp = "temp";
+const char* connect = "connect";
+const char* beep = "beep";
+
+// Function prototypes
+void uart_send_string(const char* command, UART_HandleTypeDef uart);
 
 
 
