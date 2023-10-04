5e887a6ef5ed3d28bd44d5627fc1cbc5f3d525a8
ViktorM02 <viktormann8@gmail.com>
2023-10-04 17:23:51 +0200

    Add structure to header file

--- bms_software/Core/Inc/bms_charging.h
+++ bms_software/Core/Inc/bms_charging.h
@@ -10,13 +10,34 @@
 
 enum charging_state{
 	pre_charge,
-	constant_current,
+	constant_current_one_cell,
+	constat_current_all_cells,
 	constant_volatage,
 	no_charge
 };
 
+uint8_t charging_cell = 0;
+
+struct battery_cell {
+	uint16_t voltage;
+	uint16_t current;
+	charging_state state;
+};
+
+void get_cells_charge(battery_cell cell_1, battery_cell cell_2);
+
+void get_cells_state(battery_cell cell_1, battery_cell cell_2);
+
+void choose_charging_cells(battery_cell cell_1, battery_cell cell_2);
+
 void switch_charging_state();
 
-void get_cell_charge();
+void charge_individual_cell(uint8_t cell_nbr);
+
+void charge_both_cells();
+
+void charge();
+
+
 
 #endif /* INC_BMS_CHARGING_H_ */
