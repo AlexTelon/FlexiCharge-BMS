e54f49cdeee35d984887d0713f8fb5c8244b280d
KevFord <81682161+KevFord@users.noreply.github.com>
2023-10-04 12:16:07 +0200

    Add functions for each stage of communication.

--- bms_software/Core/Inc/bms_uart_communication.h
+++ bms_software/Core/Inc/bms_uart_communication.h
@@ -12,10 +12,20 @@
 // Function prototypes
 void uart_send_string(const uint8_t* command, UART_HandleTypeDef uart, uint8_t length);
 
+void uart_send_number(uint8_t n, UART_HandleTypeDef uart);
+
 uint8_t uart_establish_connection(UART_HandleTypeDef uart);
 
 uint8_t uart_receive_ok(UART_HandleTypeDef uart);
 
+uint8_t uart_handshake(uint8_t voltage, UART_HandleTypeDef uart);
 
+uint8_t uart_init_power(UART_HandleTypeDef uart);
+
+uint8_t uart_terminate_power(UART_HandleTypeDef uart);
+
+uint8_t uart_data_temp(UART_HandleTypeDef uart, uint8_t data);
+
+uint8_t uart_data_charge(UART_HandleTypeDef uart, uint8_t data);
 
 #endif /* INC_BMS_UART_COMMUNICATION_H_ */
