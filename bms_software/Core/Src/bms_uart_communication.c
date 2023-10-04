68673b81c8749d05f85f0a2fafa002e4a3bbf44c
KevFord <81682161+KevFord@users.noreply.github.com>
2023-10-04 11:08:10 +0200

    Create functions for sending and receiving strings.
    Also add a function for establishing connection.

--- bms_software/Core/Src/bms_uart_communication.c
+++ bms_software/Core/Src/bms_uart_communication.c
@@ -13,3 +13,23 @@ void uart_send_string(const uint8_t* command, UART_HandleTypeDef uart, uint8_t l
 		HAL_UART_Transmit(&uart, &command[i], 1, 200);
 	}
 }
+
+// This function checks for a response fromt the charger, and returns a 1 if the expected "ok" command was received.
+// Otherwise it returns a 0. A 0 should be seen as a failed response.
+uint8_t uart_receive_ok(UART_HandleTypeDef uart)
+{
+	uint8_t response[2];
+
+	if(HAL_UART_Receive(&uart, response, 2, 300) == HAL_OK)
+		if((char)response[0] == 'o' && (char)response[1] == 'k')
+			return 1;
+	return 0;
+}
+
+// Function to establish connection. Sends the "connect" command and reads the response.
+uint8_t uart_establish_connection(UART_HandleTypeDef uart)
+{
+	uart_send_string("connect", uart, 7);
+
+	return uart_receive_ok(uart);
+}
