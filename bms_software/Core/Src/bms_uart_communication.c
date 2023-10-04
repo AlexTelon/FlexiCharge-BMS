1e3b9b13b3e4dc9777bc084a81b1d74f7c0873a0
KevFord <81682161+KevFord@users.noreply.github.com>
2023-10-04 07:15:54 +0200

    Add all commands to be sent over uart and write a function to transmit strings via uart.

--- bms_software/Core/Src/bms_uart_communication.c
+++ bms_software/Core/Src/bms_uart_communication.c
@@ -3,4 +3,13 @@
  *
  */
 
+#include "bms_uart_communication.h"
 
+// Function to send a string over UART.
+void uart_send_string(const char* command, UART_HandleTypeDef uart)
+{
+	for(int i = 0; command[i] != 0; i++)
+	{
+		HAL_UART_Transmit(&uart, command[i], 1, 200);
+	}
+}
