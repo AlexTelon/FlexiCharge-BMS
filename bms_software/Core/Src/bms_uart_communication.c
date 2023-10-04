3ae6e72adbb25ae1977a14ee209d75c7d3b372ab
KevFord <81682161+KevFord@users.noreply.github.com>
2023-10-04 12:16:07 +0200

    Add functions for each stage of communication.

--- bms_software/Core/Src/bms_uart_communication.c
+++ bms_software/Core/Src/bms_uart_communication.c
@@ -14,6 +14,11 @@ void uart_send_string(const uint8_t* command, UART_HandleTypeDef uart, uint8_t l
 	}
 }
 
+void uart_send_number(uint8_t n, UART_HandleTypeDef uart)
+{
+	HAL_UART_Transmit(&uart, n, 1, 200);
+}
+
 // This function checks for a response fromt the charger, and returns a 1 if the expected "ok" command was received.
 // Otherwise it returns a 0. A 0 should be seen as a failed response.
 uint8_t uart_receive_ok(UART_HandleTypeDef uart)
@@ -26,10 +31,47 @@ uint8_t uart_receive_ok(UART_HandleTypeDef uart)
 	return 0;
 }
 
-// Function to establish connection. Sends the "connect" command and reads the response.
+// Function to establish connection. Sends the "connect" command and returns the response.
 uint8_t uart_establish_connection(UART_HandleTypeDef uart)
 {
 	uart_send_string("connect", uart, 7);
+
+	return uart_receive_ok(uart);
+}
+
+uint8_t uart_handshake(uint8_t voltage, UART_HandleTypeDef uart)
+{
+	uint8_t result = 0;
+	result = uart_establish_connection(uart);
+	uart_send_string("voltage:", uart, 8);
+	uart_send_number(voltage, uart);
+	return result;
+}
+
+uint8_t uart_init_power(UART_HandleTypeDef uart)
+{
+	uart_send_string("begin", uart, 5);
+
+	return uart_receive_ok(uart);
+}
+
+uint8_t uart_terminate_power(UART_HandleTypeDef uart)
+{
+	uart_send_string("end", uart, 3);
+
+	return uart_receive_ok(uart);
+}
+
+uint8_t uart_data_temp(UART_HandleTypeDef uart, uint8_t data)
+{
+	uart_send_string("temp", uart, 4);
+
+	return uart_receive_ok(uart);
+}
+
+uint8_t uart_data_charge(UART_HandleTypeDef uart, uint8_t data)
+{
+	uart_send_string("charge", uart, 6);
 
 	return uart_receive_ok(uart);
 }
