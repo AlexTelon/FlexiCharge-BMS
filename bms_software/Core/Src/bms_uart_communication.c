5c365a82f06c7cd91a72f20d9dd367014030f86e
KevFord <81682161+KevFord@users.noreply.github.com>
2023-10-04 08:22:34 +0200

    Change datatype of commands from char to uint8_t.
    This is now a working build.

--- bms_software/Core/Src/bms_uart_communication.c
+++ bms_software/Core/Src/bms_uart_communication.c
@@ -6,10 +6,10 @@
 #include "bms_uart_communication.h"
 
 // Function to send a string over UART.
-void uart_send_string(const char* command, UART_HandleTypeDef uart)
+void uart_send_string(const uint8_t* command, UART_HandleTypeDef uart, uint8_t length)
 {
-	for(int i = 0; command[i] != 0; i++)
+	for(int i = 0; i <= length; i++)
 	{
-		HAL_UART_Transmit(&uart, command[i], 1, 200);
+		HAL_UART_Transmit(&uart, &command[i], 1, 200);
 	}
 }
